133cb870e8e00ed55ef70f68f25398f9bdf71000
GOUDA\Mehrab <mehrabmodi@gmail.com>
2017-02-06 15:02:27 -0500

    Plotting prettiness functions. fig_wrapup.m resizes, removes bounding boxes for figures. venn.m plots venn diagrams. add_stim_bar adds a stimulus indicator bar outside and above the figure.

---
+++ fig_wrapup.m
@@ -0,0 +1,29 @@
+function [] = fig_wrapup(fig_n)
+%This function changes figure window size, gets rid of top and right side
+%grid lines, adjusts ticklabel font size, and changes size, orientation of
+%axis ticks.
+
+% %testing lines
+% vec = rand(1, 100);
+% fig_h = figure(1);
+% fig_n = 1;
+% plot(vec)
+
+
+plot_height = 200;
+plot_width = 280;
+axis_font_size = 15;
+tick_length = [0.02, 0.05];
+line_widths = 2;
+marker_sizes = 1;
+marker_sizes_f = 4;
+
+fig_h = figure(fig_n);
+set(fig_h, 'Position', [100, 100, 100 + plot_width, 100 + plot_height]);
+ax_handle = gca;
+ax_handle.FontSize = axis_font_size;
+ax_handle.TickLength = [0.005, 0.005];
+ax_handle.Box = 'off';
+ax_handle.TickDir = 'out';
+
+end
