cb7c5222f593b4144b7702251085546f8fb813f5
GOUDA\Mehrab <mehrabmodi@gmail.com>
2017-12-13 13:50:26 -0500

    Edited this function to make the make_db and master_file file it generates compatible with the newest version of Suite2P. Minor changes mostly to do with line numbers etc.

--- Use_Suite2P_mehrab/mehrab_scripts/setup_Suite2P_files.m
+++ Use_Suite2P_mehrab/mehrab_scripts/setup_Suite2P_files.m
@@ -4,7 +4,6 @@ function [ft_factor] = setup_Suite2P_files(raw_direc_base, raw_direc, m_db_file_
 %It also edits both files according to the dataset in the current
 %raw_direc.
 
-
     %creating make_db and master_file as needed to run Suite2P
     prev_direc = pwd;
     cd([raw_direc_base, raw_direc])
@@ -16,7 +15,7 @@ function [ft_factor] = setup_Suite2P_files(raw_direc_base, raw_direc, m_db_file_
     fprintf(fid, ['i = 1;', '\n']);
     fprintf(fid, ['db(i).mouse_name    = ''' raw_direc(1:bslashi(1) ) ''';' '\n']);
     fprintf(fid, ['db(i).date          = ''' raw_direc((bslashi(1)+1):bslashi(2)) ''';' '\n']);
-    fprintf(fid, ['db(i).expts         = {''' raw_direc((bslashi(2)+1):bslashi(3)) '''};' '\n']);
+    fprintf(fid, ['db(i).expts         = [''' raw_direc((bslashi(2)+1):bslashi(3)) '''];' '\n']);
     fprintf(fid, 'db(i).nchannels     = 1;\n');
     fprintf(fid, 'db(i).gchannel      = 1;\n');
     fprintf(fid, 'db(i).nplanes       = 1;\n');
@@ -28,8 +27,6 @@ function [ft_factor] = setup_Suite2P_files(raw_direc_base, raw_direc, m_db_file_
     %This funtion copies over master_file.m from the path given to it as
     %one of it's inputs. This typically needs to be edited very rarely.
     copyfile(m_db_file_direc, [raw_direc_base, raw_direc]);
-    prev_direc = pwd;
-    cd([raw_direc_base, raw_direc])
     
     %reading in frame rate and editing local master_file to reflect it
     dir_contents = dir('*.tif');
@@ -47,7 +44,7 @@ function [ft_factor] = setup_Suite2P_files(raw_direc_base, raw_direc, m_db_file_
     
     %editing masterfile with parameters for current dataset
     master_file = regexp(fileread('master_file.m'), '\n', 'split');
-    fr_line = master_file{46};
+    fr_line = master_file{38};
     
     frame_time_factor = num2str(1./frame_time, 2);
     try
@@ -55,7 +52,7 @@ function [ft_factor] = setup_Suite2P_files(raw_direc_base, raw_direc, m_db_file_
     catch
         keyboard
     end
-    master_file{46} = fr_line;
+    master_file{38} = fr_line;
     
     fid = fopen('master_file.m', 'w');
     fprintf(fid, '%s\n', master_file{:});
