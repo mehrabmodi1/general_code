01fad818ea49112b081d78f8f37ac55cf67cb28f
Mehrab Modi <mehrabmodi@gmail.com>
2017-12-27 12:02:52 -0500

    Small alteration in how the precision is selected for the new xlabels.

--- set_xlabels_time.m
+++ set_xlabels_time.m
@@ -1,5 +1,6 @@
 function [] = set_xlabels_time(fig_n, frame_time, n_ticks_multiplier)
 %syntax:[] = set_xlabels_time(fig_n, frame_time, n_ticks_multiplier)
+%Note: Specify frame_time in s.
 
 figure(fig_n)
 axis_m = axis;
@@ -7,10 +8,9 @@ axis_m = axis;
 n_frames = floor(axis_m(1, 2));
 tot_time = n_frames.*frame_time;
 range_n = floor(log10(tot_time));   %10 raised to range_n = tot_time, useful for rounding off t_step
-tot_time_rounded = round(tot_time, (range_n-1));
+tot_time_rounded = round(tot_time, (range_n-2));
 n_ticks = round(((tot_time_rounded./(10.^range_n)).*2).*n_ticks_multiplier);
-
-t_step = round(tot_time_rounded./n_ticks, (range_n-1) );
+t_step = round(tot_time_rounded./n_ticks, (range_n-2) );
 
 x_tick_labelsi = 0:t_step:tot_time;
 x_ticks = x_tick_labelsi./frame_time;
