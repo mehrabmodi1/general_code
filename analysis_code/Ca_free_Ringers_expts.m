529cb7299055faa7f85643c430e49ec49df6dd7f
Mehrab Modi <mehrabmodi@gmail.com>
2018-04-24 18:04:05 -0400

    minor changes.

--- analysis_code/Ca_free_Ringers_expts.m
+++ analysis_code/Ca_free_Ringers_expts.m
@@ -1,3 +1,16 @@
 clear all
 close all
 
+direc_list_path = 'C:\Data\Data\Raw_data\dataset_lists\dataset_list_Cafree_Ringers_expt.xls';
+[del, direc_list] = xlsread(direc_list_path, 1);
+n_direcs = size(cell_direc_list, 1);
+
+for direc_n = 1:n_direcs
+    curr_dir = direc_list{direc_n, 1};
+    
+    dir_contents = dir_date_sorted(curr_dir, '*.tif');
+    
+    
+    keyboard
+    
+end
