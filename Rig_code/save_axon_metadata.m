7c13fc645afccb575101392d224c74d77cc5c16f
GANDALF-WW1\scanimage <mehrabmodi@gmail.com>
2018-05-09 16:03:40 -0400

    minor changes.

--- Rig_code/save_axon_metadata.m
+++
@@ -1,5 +0,0 @@
-function metadata = save_axon_metadata()
-%This function uses MulticlampTelegraph to get amp electrode parameters
-%from the amplifier and adds them to the structure that will be saved by
-%WaveSurfer in it's data file.
-
