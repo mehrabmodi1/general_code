df892df9a0571bdb6605216d52171dd4a215206f
Mehrab Modi <mehrabmodi@gmail.com>
2018-05-17 17:46:15 -0400

    insignificant stuff.

--- scrap.m
+++ scrap.m
@@ -4,5 +4,11 @@ close all
 path = 'C:\Data\Data\Analysed_data\Suite2P_results\20180514\odpulse_freq_series\1\odor_trs_00003.tif';
 
 frame_orig = imread(path, 1);
+frame_orig_rescaled = frame_orig./65535;
+
 
 frame = im2double(frame_orig);
+frame = frame - 0.5;
+
+diff = frame_orig_rescaled - frame_orig;
+imagesc(diff)
