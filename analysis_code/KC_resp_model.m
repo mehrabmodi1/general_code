143b56ab1866625670aab3821ef45e9c9bcfd391
Mehrab Modi <mehrabmodi@gmail.com>
2018-05-22 17:23:41 -0400

    work in progress.

---
+++ analysis_code/KC_resp_model.m
@@ -0,0 +1,14 @@
+clear all
+close all
+
+%model parameters
+on_tau = 0.1;
+on_amplit = 2;
+on_dec_tau = 0.5;
+sus_amplit = 1;
+off_tau = 0.1;
+off_amplit = 1;
+off_dec_tau = 2;
+
+%input values
+od_vec = 
