ff2b3d7b5ab1b8b28b79e611fe8a79b1ca0b442c
Mehrab Modi <mehrabmodi@gmail.com>
2018-05-22 18:26:53 -0400

    updated function to plot traces along with stim bar.

--- plot_traces_simple.m
+++ plot_traces_simple.m
@@ -1,12 +1,19 @@
-function [h, max_val] = plot_traces_simple(trace_mat, time_vec, norm, plot_mean)
+function [h, max_val] = plot_traces_simple(fig_n, dff_data_mat, cell_n, odor_n, dur_n, stim_mat_simple, stim_mat, frame_time, color_vec, norm, plot_mean)
+%syntax: function [h, max_val] = plot_traces_simple(fig_n, dff_data_mat, cell_n, odor_n, dur_n, stim_mat_simple, stim_mat, frame_time, norm, plot_mean)
 %This function plots single trial dF/F traces in grey with the averaged
 %trace in a thicker black. It plots traces for a single cell, for a chosen
 %odor number or across all odors.
-%Mehrab Modi, 10/3/2014
+%Mehrab Modi, 20180522
 
-close(findobj('type','figure','name','dF/F Traces'))
+odor_list = unique(stim_mat_simple(:, 2));
+odor_dur_list = unique(stim_mat_simple(:, 3));
+curr_dur = odor_dur_list(dur_n);
+odor_ni = odor_list(odor_n);
 
-cell_traces = trace_mat;
+curr_trs = find(stim_mat_simple(:, 2) == odor_ni & stim_mat_simple(:, 3) == curr_dur);
+stim_frs = compute_stim_frs(stim_mat, curr_trs(1), frame_time);
+
+cell_traces = squeeze(dff_data_mat(:, cell_n, curr_trs));
 ave_trace = mean(cell_traces, 2, 'omitnan');
 
 max_val = max(ave_trace);
@@ -19,14 +26,17 @@ else
 end
 
 grey = [0.6, 0.6, 0.6];
-h = plot(time_vec, cell_traces);
+figure(fig_n)
+h = plot(cell_traces);
 set(h, 'Color', grey);
 if plot_mean == 1
     hold on
-    plot(time_vec, ave_trace, 'k', 'LineWidth', 3);
+    plot(ave_trace, 'k', 'LineWidth', 3);
     hold off
 else
 end
 set(gcf, 'Color', 'w')
+set_xlabels_time(fig_n, frame_time, 5);
+add_stim_bar(fig_n, stim_frs, color_vec(odor_n, :))
 
 end
