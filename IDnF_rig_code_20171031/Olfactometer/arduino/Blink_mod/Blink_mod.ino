87948e341810a2fadb5d2e8bed4bb7d80271ff6f
GANDALF-WW1\scanimage <mehrabmodi@gmail.com>
2018-07-09 11:52:48 -0400

    minor changes.

--- IDnF_rig_code_20171031/Olfactometer/arduino/Blink_mod/Blink_mod.ino
+++ IDnF_rig_code_20171031/Olfactometer/arduino/Blink_mod/Blink_mod.ino
@@ -24,13 +24,13 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(4, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(100);                       // wait for a second
-  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(4, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
