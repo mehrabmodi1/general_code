ad8e1171a8f9ef05e72ff55d2cbe744310afed2e
Mehrab Modi <mehrabmodi@gmail.com>
2018-07-16 17:46:54 -0400

    set up so script is plotting response matrix.

--- analysis_code/Yoshi_PaBaEl_MBON_pairing.m
+++ analysis_code/Yoshi_PaBaEl_MBON_pairing.m
@@ -1,8 +1,74 @@
 clear all
 close all
 
-direc_list_mat = [...
-                    {'C:\Data\Data\Analysed_data\Manual_ROI_results\20180713\fly2_MB310CxopGC6f_72D01LxA_LxAChr88tdt\dataset_list_Yoshi_PaBaEl_MBONalpha1.xls'};...
+dataset_list_paths = [...
+                    {'C:\Data\Data\Analysed_data\dataset_lists\dataset_list_Yoshi_PaBaEl_MBONalpha1.xls'};...
 
 
                 ];
+            
+suppress_plots = 1;
+[del, odor_names] = xlsread('C:\Data\Code\general_code\IDnF_rig_code_20171031\Olfactometer\NewOlfactometer\calibration\odorList.xls', 1);
+a = colormap('bone');
+global greymap
+greymap = flipud(a);
+
+
+for list_n = 1:size(dataset_list_paths, 1)
+    curr_dir_list_path = dataset_list_paths{list_n, 1};
+    [del, dir_list] = xlsread(curr_dir_list_path, 1);        %list of Suite2P results directories
+    n_dirs = size(dir_list, 1);
+    dataset_list_name = findstr(curr_dir_list_path, 'El_');
+    dataset_list_name = curr_dir_list_path((dataset_list_name + 1):(end - 4));
+    dataset_list_name(1) = [];
+    
+    %loop to go through all experiment datasets listed in list file
+    for dir_n = 1:n_dirs
+       saved_an_results.scriptname = mfilename('fullpath');
+       curr_dir = [dir_list{dir_n, 1}, '\'];
+       tif_times = load([curr_dir, 'tif_time_stamps.mat']);           %reading in time stamps for each tif file recorded by raw_data_extracter
+       tif_times = tif_times.time_stamps;
+       [stim_mat, stim_mat_simple, column_heads, color_vec] = load_params_trains(curr_dir, tif_times);    %reading in trial stimulus parameters after matching time stamps to F traces
+       
+       %Reading in experimental parameters
+        odor_list = unique(stim_mat_simple(:, 2) );
+        n_odors = length(odor_list);
+        odor_dur_list = unique(stim_mat_simple(:, 3) );
+        n_od_durs = length(odor_dur_list);
+        n_trains = max(stim_mat_simple(:, 11));
+        saved_an_results.odor_list = odor_list;
+        saved_an_results.odor_dur_list = odor_dur_list;
+        
+        cd(curr_dir);
+        tif_name = dir('*.tif');
+        stack_obj = ScanImageTiffReader([curr_dir, tif_name(1).name]);
+        [frame_time, zoom, n_chans, PMT_offsets] = SI_tif_info(stack_obj);
+        
+        
+        %loading extracted raw fluorescence data matrices written by raw_dff_extractor
+        raw_data_mat = load([curr_dir 'extracted_raw_data_mat.mat']);
+        raw_data_mat = raw_data_mat.raw_data_mat;           %raw F traces extracted from ROIs
+        raw_data_mat_orig = raw_data_mat;
+        raw_data_mat = raw_data_mat(:, :, stim_mat_simple(:, 1));       %making sure only time-stamp matched trials are used for further analysis
+        n_cells = size(raw_data_mat, 2);
+
+        %calculating dF/F traces from raw data
+        filt_time = 0.5;            %in s, the time window for boxcar filter for generating filtered traces
+        [dff_data_mat, dff_data_mat_f] = cal_dff_traces_res(raw_data_mat, stim_mat, frame_time, filt_time, curr_dir);
+        del = find(dff_data_mat_f < -1);
+        dff_data_mat_f(del) = -1;       %forcing crazy values to sane ones
+        
+        %identifying significantly responsive cells
+        [resp_areas, sig_trace_mat, sig_trace_mat_old, sig_cell_mat] = cal_sig_responses_res(dff_data_mat, stim_mat, stim_mat_simple, curr_dir, frame_time);
+        sig_cells = find(sum(sum(sig_cell_mat, 3), 2) > 0);         %list of all cells significant for any odor for any duration
+        
+        imagesc(squeeze(dff_data_mat(:, 1, :))', [0, 1])
+        
+        keyboard
+       
+       
+       
+    end
+    
+end
+        
