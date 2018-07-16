11f8e91342292b4119390f786a3788be99faefa3
Mehrab Modi <mehrabmodi@gmail.com>
2018-07-16 16:57:05 -0400

    started work on analysis script for MBON imaging, DAN-odor pairing experiments.

---
+++ analysis_code/Yoshi_PaBaEl_MBON_pairing.m
@@ -0,0 +1,8 @@
+clear all
+close all
+
+direc_list_mat = [...
+                    {'C:\Data\Data\Analysed_data\Manual_ROI_results\20180713\fly2_MB310CxopGC6f_72D01LxA_LxAChr88tdt\dataset_list_Yoshi_PaBaEl_MBONalpha1.xls'};...
+
+
+                ];
