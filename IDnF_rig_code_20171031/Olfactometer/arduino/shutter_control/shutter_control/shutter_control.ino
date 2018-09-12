5e0cb3435ae657a6f4fea8bce55ca113b2673762
GANDALF-WW1\scanimage <mehrabmodi@gmail.com>
2018-09-12 14:56:21 -0400

    Updated shutter control Arduino to handle red and green channels independently.

--- IDnF_rig_code_20171031/Olfactometer/arduino/shutter_control/shutter_control/shutter_control.ino
+++ IDnF_rig_code_20171031/Olfactometer/arduino/shutter_control/shutter_control/shutter_control.ino
@@ -4,32 +4,43 @@
 
 // constants won't change. They're used here to
 // set pin numbers:
-const int ScanImagePMTPin = 2;     // the number of the pushbutton pin
-const int LEDWarningPin = 4;     // the number of the pushbutton pin
-const int shutterOutPin =  6;      // the number of the LED pin
+const int ScanImagePMTPinG = 2;     // Scanimage control signal in (green)  
+const int ScanImagePMTPinR = 3;     // Scanimage control signal in (red)
+const int LEDWarningPin = 4;        // LED warning signal in from stim LED controller
+const int shutterOutPinG =  6;      // signal out to shutter controller (green)
+const int shutterOutPinR =  7;      // signal out to shutter controller (red)
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+
 
 void setup() {
-  // initialize the shutter control pin as an output:
-  pinMode(shutterOutPin, OUTPUT);
-  // initialize the SI PMT input and stim LED warning pins as an inputs:
-  pinMode(ScanImagePMTPin, INPUT);
+  // initialize the shutter control pins as an outputs:
+  pinMode(shutterOutPinG, OUTPUT);
+  pinMode(shutterOutPinR, OUTPUT);
+  // initialize the SI PMT inputs and stim LED warning pins as an inputs:
+  pinMode(ScanImagePMTPinG, INPUT);
+  pinMode(ScanImagePMTPinR, INPUT);
   pinMode(LEDWarningPin, INPUT);
 }
 
 void loop() {
   // read the state of the pushbutton value:
- bool SIstate = digitalRead(ScanImagePMTPin);
+ bool SIstateG = digitalRead(ScanImagePMTPinG);
+ bool SIstateR = digitalRead(ScanImagePMTPinR);
  bool LEDstate = digitalRead(LEDWarningPin);
   
-  // opening shutter only if LEDstate is low and SIstate is high
-  if (SIstate == HIGH && LEDstate == LOW) {
+  // opening G_shutter only if LEDstate is low and G_SIstate is high
+  if (SIstateG == HIGH && LEDstate == LOW) {
     // opening shutter:
-    digitalWrite(shutterOutPin, HIGH);
+    digitalWrite(shutterOutPinG, HIGH);
   } else {
-    // turn LED off:
-    digitalWrite(shutterOutPin, LOW);
+    digitalWrite(shutterOutPinG, LOW);
+  }
+
+  // opening R_shutter only if LEDstate is low and G_SIstate is high
+  if (SIstateR == HIGH && LEDstate == LOW) {
+    // opening shutter:
+    digitalWrite(shutterOutPinR, HIGH);
+  } else {
+    digitalWrite(shutterOutPinR, LOW);
   }
 }
