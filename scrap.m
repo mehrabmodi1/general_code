2d40d2153e33d8765bff17c9e7e369acf1da3b5d
mehrab modi <mehrabmodi@gmail.com>
2019-01-03 12:14:22 -0500

    Minor changes.

--- scrap.m
+++ scrap.m
@@ -1,12 +1,15 @@
 clear all
 close all
-a = 0
-for i = 1:10
-    
-    a = a+ 1;
-    
-    if i == 5
-        i = 7;
-    else
-    end
-end
+figure(1)
+[del, img1] = vennX_MM( [ 20 10 20 10 20 10 10], .01, [], 0, 1, 1);
+alpha(0.5)
+hold on
+
+[del, img2] = vennX_MM( [ 25 10 15 10 20 10 10], .01, [], 0, 1, 1 );
+alpha(0.5)
+
+figure(2)
+a = imagesc(img1);
+hold on
+b = imagesc(img2);
+alpha(b, 0.5)
