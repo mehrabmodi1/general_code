ada29efe9f76d7b5ce634aa54bc596a5140a2981
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2019-02-28 11:40:45 -0500

    more minor changes.

--- scrap2.m
+++ scrap2.m
@@ -2,7 +2,8 @@ clear all
 close all
 figure(1)
 
-plot(rand(10, 10).*100)
+imagesc(rand(10, 10).*100)
 script_name = mfilename;
 xlabel('xlabel')
-fig_wrapup(1, script_name);
+ylabel('ylabel')
+fig_wrapup(1, script_name)
