d23c7de358aa02b0e10a8a9ec92543d6c3584079
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2019-02-28 11:41:42 -0500

    Wrote code to make odor-odor scatter plots with 80percentile bound lines

--- analysis_code/PaBaEl_KC_pop_analysis.m
+++ analysis_code/PaBaEl_KC_pop_analysis.m
@@ -15,7 +15,7 @@ suppress_plots = 0;
 a = colormap('bone');
 global greymap
 greymap = flipud(a);
-
+script_name = mfilename;
 
 for list_n = 1:size(dataset_list_paths, 1)
     curr_dir_list_path = dataset_list_paths{list_n, 1};
@@ -81,13 +81,14 @@ for list_n = 1:size(dataset_list_paths, 1)
         
         
             %plotting repeat-ave KC pop resp traces to each odor
-            KCpop_odor_resp_plotter(dff_data_mat_f, stim_mat_simple, stim_mat, sig_cell_mat, frame_time, 1);
+            KCpop_odor_resp_plotter(dff_data_mat_f, stim_mat_simple, stim_mat, sig_cell_mat, frame_time, 1, script_name);
 
         else
         end
         %pooling resp data across flies
         
         ave_resp_areas = zeros(size(dff_data_mat, 2), length(odor_list)) + nan;
+        
         for odor_n = 1:length(odor_list)
             odor_ni = odor_list(odor_n);
             curr_trs = find(stim_mat_simple(:, 2) == odor_ni);
@@ -116,11 +117,14 @@ for list_n = 1:size(dataset_list_paths, 1)
         %storing venn diagram matrices
 %         [del, img] = vennX_MM([n_responders1, n_responders1_2, n_responders2, n_responders2_3, n_responders3, n_responders1_3, n_responders1_2_3]./n_cells, 0.005, [], 0, 1, 0);
 %         venn_mats = pad_n_concatenate(venn_mats, img, 3, 0);
-        keyboard
+        %keyboard
         close figure 1
         close figure 2
         close figure 3
     end
+    
+    big_resp_areas_all(list_n).big_resp_areas = big_resp_areas;
+    big_sig_cell_mat_all(list_n).big_sig_cell_mat = big_sig_cell_mat;
     
     %identifying sig responders to each odor and plotting their mean resp areas for each odor
     %loop to generate plots for resps to each odor
@@ -160,7 +164,7 @@ for list_n = 1:size(dataset_list_paths, 1)
         xlabel('dF/F response size');
         ylabel('norm. counts')
         legend('PA responders', 'BA responders', 'EL responders') 
-        fig_wrapup(plot_odor_n)
+        fig_wrapup(plot_odor_n, script_name)
         
     end
     
@@ -168,7 +172,7 @@ for list_n = 1:size(dataset_list_paths, 1)
     %plotting ovlap counts
     scattered_dot_plot(ovlap_counts, 4, 2, 3, 8, [0.6, 0.6, 0.6], 0, [], [0, 0, 0], [{'PA-BA'}, {'PA-EL'}, {'BA-EL'}], 1, [1, 0.2, 0.2])
     ylabel('overlap fraction');
-    fig_wrapup(4)
+    fig_wrapup(4, script_name)
     
     [h1, p1] = ttest(ovlap_counts(:, 1), ovlap_counts(:, 2))
     [h2, p2] = ttest(ovlap_counts(:, 1), ovlap_counts(:, 3))
@@ -194,3 +198,87 @@ for list_n = 1:size(dataset_list_paths, 1)
     close figure 4
     close figure 5
 end
+
+%making resp-size scatter plot
+G_resps = big_resp_areas_all(1).big_resp_areas;
+G_resps = G_resps./repmat(G_resps(:, 2), 1, 3);           %normalising each cell's resps to max across odours
+G_sig_vec = sum(big_sig_cell_mat_all(1).big_sig_cell_mat, 2);
+G_sig_list = find(G_sig_vec > 0);
+
+AB_resps = big_resp_areas_all(2).big_resp_areas;
+AB_resps = AB_resps./repmat(AB_resps(:, 2), 1, 3);         %normalising each cell's resps to max across odours
+AB_sig_vec = sum(big_sig_cell_mat_all(2).big_sig_cell_mat, 2);
+AB_sig_list = find(AB_sig_vec > 0);
+
+G_colour = [0.13, 0.44, 0.72];
+AB_colour = [0.13, 0.71, 0.52];
+figure(7)
+plot(AB_resps(AB_sig_list, 1), AB_resps(AB_sig_list, 3), '.', 'markerSize', 6, 'markerEdgeColor', AB_colour);
+hold on
+plot(G_resps(G_sig_list, 1), G_resps(G_sig_list, 3), '.', 'markerSize', 6, 'markerEdgeColor', G_colour);
+pbaspect([1, 1, 1]);
+xlabel('norm PA responses')
+ylabel('norm EL responses')
+axis([0, 4, 0, 4])
+
+%finding 90% resp slope boundaries
+resp_ratios = AB_resps(:, 1)./AB_resps(:, 3);
+low_quantile = quantile(resp_ratios, .1);
+high_quantile = quantile(resp_ratios, .90);
+high_quant_vals = [0, 0; 8.*high_quantile, 8]; 
+low_quant_vals = [0, 0; 8.*low_quantile, 8];
+plot(high_quant_vals(:, 1), high_quant_vals(:, 2), '-', 'lineWidth', 2, 'Color', AB_colour )
+plot(low_quant_vals(:, 1), low_quant_vals(:, 2), '-', 'lineWidth', 2, 'Color', AB_colour )
+
+resp_ratios = G_resps(:, 1)./G_resps(:, 3);
+low_quantile = quantile(resp_ratios, .1);
+high_quantile = quantile(resp_ratios, .90);
+high_quant_vals = [0, 0; 8.*high_quantile, 8]; 
+low_quant_vals = [0, 0; 8.*low_quantile, 8];
+plot(high_quant_vals(:, 1), high_quant_vals(:, 2), '-', 'lineWidth', 2, 'Color', G_colour )
+plot(low_quant_vals(:, 1), low_quant_vals(:, 2), '-', 'lineWidth', 2, 'Color', G_colour )
+fig_wrapup(7, script_name)
+
+
+%making resp-size scatter plot 2
+G_resps = big_resp_areas_all(1).big_resp_areas;
+G_resps = G_resps./repmat(G_resps(:, 3), 1, 3);           %normalising each cell's resps to max across odours
+G_sig_vec = sum(big_sig_cell_mat_all(1).big_sig_cell_mat, 2);
+G_sig_list = find(G_sig_vec > 0);
+
+AB_resps = big_resp_areas_all(2).big_resp_areas;
+AB_resps = AB_resps./repmat(AB_resps(:, 3), 1, 3);         %normalising each cell's resps to max across odours
+AB_sig_vec = sum(big_sig_cell_mat_all(2).big_sig_cell_mat, 2);
+AB_sig_list = find(AB_sig_vec > 0);
+
+
+figure(8)
+plot(AB_resps(AB_sig_list, 1), AB_resps(AB_sig_list, 2), '.', 'markerSize', 6, 'markerEdgeColor', AB_colour);
+hold on
+plot(G_resps(G_sig_list, 1), G_resps(G_sig_list, 2), '.', 'markerSize', 6, 'markerEdgeColor', G_colour);
+pbaspect([1, 1, 1]);
+xlabel('norm PA responses')
+ylabel('norm BA responses')
+axis([0, 4, 0, 4])
+
+%finding 90% resp slope boundaries
+resp_ratios = AB_resps(:, 1)./AB_resps(:, 2);
+low_quantile = quantile(resp_ratios, .1);
+high_quantile = quantile(resp_ratios, .90);
+high_quant_vals = [0, 0; 8.*high_quantile, 8]; 
+low_quant_vals = [0, 0; 8.*low_quantile, 8];
+plot(high_quant_vals(:, 1), high_quant_vals(:, 2), '-', 'lineWidth', 2, 'Color', AB_colour )
+plot(low_quant_vals(:, 1), low_quant_vals(:, 2), '-', 'lineWidth', 2, 'Color', AB_colour )
+
+resp_ratios = G_resps(:, 1)./G_resps(:, 2);
+low_quantile = quantile(resp_ratios, .1);
+high_quantile = quantile(resp_ratios, .90);
+high_quant_vals = [0, 0; 8.*high_quantile, 8]; 
+low_quant_vals = [0, 0; 8.*low_quantile, 8];
+plot(high_quant_vals(:, 1), high_quant_vals(:, 2), '-', 'lineWidth', 2, 'Color', G_colour )
+plot(low_quant_vals(:, 1), low_quant_vals(:, 2), '-', 'lineWidth', 2, 'Color', G_colour )
+fig_wrapup(8, script_name)
+
+
+
+
