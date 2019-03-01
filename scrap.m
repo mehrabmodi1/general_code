165a74617c467b7db63b6d25c899b3110377286c
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2019-03-01 14:01:20 -0500

    minor testing.

--- scrap.m
+++ scrap.m
@@ -1,56 +1,18 @@
-naive = [0.6, 1, 0.8, 0.7, 0.9, 0.7, 0.9, 0.9, 1, 0.8];
-naive = naive';
-vecs = repmat(naive, 1, 8);
+clear all
+close all
 
-vecs(2, 2) = 0.7;
-vecs(7, 2) = 0.6;
-vecs(10, 2) = 0.6;
+f = figure;
+axis([0, 10, 0, 5])
 
-vecs(2, 3) = 0.7;
-vecs(7, 3) = 0.6;
-vecs(10, 3) = 0.6;
-vecs(3, 3) = 0.9;
-vecs(5, 3) = 1;
-vecs(9, 3) = 1;
+greenrect = rectangle('Position',[0.7 0.1 0.1 0.2]);
+set(greenrect,'FaceColor','g');
+draggable(greenrect);
 
-vecs(2, 4) = 0.5;
-vecs(7, 4) = 0.4;
-vecs(10, 4) = 0.4;
-vecs(3, 4) = 0.9;
-vecs(5, 4) = 1;
-vecs(9, 4) = 1;
+hold on
 
-vecs(3, 5) = 1;
-vecs(5, 5) = 1;
-vecs(9, 5) = 1;
-vecs(2, 5) = 0.5;
-vecs(7, 5) = 0.4;
-vecs(10, 5) = 0.4;
+a = rand(2, 2) + 1;
+im = imagesc(a);
+draggable(im, 'none', [-inf inf -inf inf]);
 
-vecs(2, 6) = 0.4;
-vecs(7, 6) = 0.3;
-vecs(10, 6) = 0.3;
-vecs(3, 6) = 1;
-vecs(5, 6) = 1;
-vecs(9, 6) = 1;
-
-vecs(2, 7) = 0.4;
-vecs(7, 7) = 0.3;
-vecs(10, 7) = 0.3;
-vecs(3, 7) = 1;
-vecs(5, 7) = 1;
-vecs(9, 7) = 1;
-
-vecs(2, 8) = 0.0;
-vecs(7, 8) = 0.0;
-vecs(10, 8) = 0.0;
-vecs(3, 8) = 1;
-vecs(5, 8) = 1;
-vecs(9, 8) = 1;
-
-a = pdist(vecs');
-a = squareform(a);
-
-figure(1)
-plot(a(:, 1), 'k', 'lineWidth', 3)
-fig_wrapup(1)
+im.XData
+im.YData
