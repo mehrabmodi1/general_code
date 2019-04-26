afccd5b09a41cbd3f2186ff4ac5431598030734a
mehrab modi <mehrabmodi@gmail.com>
2019-04-26 15:51:36 -0400

    wrote a function to put olf2 to sleep.

---
+++ IDnF_rig_code_20171031/Olfactometer/NewOlfactometer/sleep_olf2.m
@@ -0,0 +1,13 @@
+function [] = sleep_olf2()
+
+close_serial_port(13)
+olf_arduino_serial_comm(0, [.5, .5], 1, 1)
+pause(.5)
+trigger_scan(1)
+pause(.5)
+trigger_scan(0)
+olf_arduino_serial_comm(2, [.5, .5], 1, 1)
+pause(.5)
+trigger_scan(1)
+pause(.5)
+trigger_scan(0)
