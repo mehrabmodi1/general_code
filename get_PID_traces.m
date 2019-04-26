c70b78e0b08f598b6b3d4f01f58ca24e314f0ac7
mehrab modi <mehrabmodi@gmail.com>
2019-04-26 15:51:58 -0400

    updated to handle the acquired LED-stim traces as well.

--- get_PID_traces.m
+++ get_PID_traces.m
@@ -15,23 +15,30 @@ for trial_n = 1:n_trials
     trial_ni = curr_trs(trial_n);
     curr_trace = load([direc, 'PID_trace_tr-', num2str(trial_ni)]);
     curr_trace = curr_trace.PID_data;
-    
+      
     if trial_n == 1
-        acqn_time = mean(diff(curr_trace(:, 2)));
+        acqn_time = mean(diff(curr_trace(:, 3)));
         fr_pt_ratio = round(frame_time./acqn_time);
     else
     end
     
     %subtracting baseline
-    baseline = mean(curr_trace(1:500, 1));  %this assumes no odor was delivered in the first half second of the trace
-    curr_trace(:, 1) = curr_trace(:, 1) - baseline;
+    baseline = mean(curr_trace(400:500, 1:2), 1);  %this assumes no odor was delivered in the first half second of the trace
+    curr_trace(:, 1:2) = curr_trace(:, 1:2) - repmat(baseline, size(curr_trace, 1), 1);
     
     %downsampling PID trace to match frame rate
-    points_to_sample = linspace(1, max(curr_trace(:, 2)), size(curr_trace, 1)./fr_pt_ratio);
-    curr_trace_ds = interp1(curr_trace(:, 2), curr_trace(:, 1), points_to_sample, 'linear');
-    
-    PID_traces = [PID_traces; curr_trace_ds];
-    
+    x = curr_trace(:, 1);
+    n = fr_pt_ratio;
+    m = numel(x);
+    curr_trace_ds(:, 1) = mean(reshape( [x(:);nan(mod(-m,n),1)],n,[]), 'omitnan');
+    x = curr_trace(:, 2);
+    n = fr_pt_ratio;
+    m = numel(x);
+    curr_trace_ds(:, 2) = mean(reshape( [x(:);nan(mod(-m,n),1)],n,[]), 'omitnan');
+    curr_trace_ds(1:4, :) = 0;
+    %curr_trace_ds = interp1(curr_trace(:, 3), curr_trace(:, 1), points_to_sample, 'linear');
+    PID_traces = [PID_traces, curr_trace_ds];
+  
 end
 %normalising while retaining relative differences between repeats
 PID_traces = PID_traces./max(max(PID_traces));
