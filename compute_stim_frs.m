d915a9680b6780b113afa770c97737415799cb13
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2019-05-07 11:03:32 -0400

    minor changes.

--- compute_stim_frs.m
+++ compute_stim_frs.m
@@ -1,5 +1,5 @@
 function [stim_frs] = compute_stim_frs(stim_mat, trial_n, frame_time)
 
 curr_train = stim_mat(trial_n).rand_trains;      %this applies even for trials that are not random train trials.
-stim_latency = stim_mat(trial_n).stim_latency;
+stim_latency = stim_mat(trial_n).stimLatency;
 stim_frs = compute_pulse_frames_train(curr_train, frame_time, stim_latency);
