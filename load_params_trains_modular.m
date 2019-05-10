e29c9dbf5879efaa3009995c58dea34dbbd2e299
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2019-05-10 14:37:46 -0400

    MInor changes plus switched to color brewer function to generate colours.

--- load_params_trains_modular.m
+++ load_params_trains_modular.m
@@ -164,9 +164,7 @@ save([direc, '\stim_mat.mat'], 'stim_mat')
 
 %setting up the standard color vector to use for plotting depending on how
 %many odors there are in this dataset
-n_odors = length(unique(stim_mat_simple(:, 2))); 
-color_vec = setup_std_color_vec(n_odors);
-
-
+n_odors = length(unique(stim_mat_simple(:, 1))); 
+color_vec = cbrewer('qual', 'Pastel1' ,n_odors, 'cubic');
 
 end
