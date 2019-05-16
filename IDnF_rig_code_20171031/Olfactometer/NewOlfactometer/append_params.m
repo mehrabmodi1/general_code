0c3f9025df99cda4aa16cc217579ef2c79c1eaa0
mehrab modi <mehrabmodi@gmail.com>
2019-05-16 12:04:50 -0400

    minor changes.

--- IDnF_rig_code_20171031/Olfactometer/NewOlfactometer/append_params.m
+++ IDnF_rig_code_20171031/Olfactometer/NewOlfactometer/append_params.m
@@ -1,13 +1,12 @@
 function [params_struc_fused] = append_params(params_mat1, params_mat2, randomise)
 
-n_trs1 = size(params_mat1, 2);
-n_trs2 = size(params_mat2, 2);
-
-
 params_struc_fused = [params_mat1, params_mat2];
 
 %randomising trials in fused param matrix if specified
 if randomise == 1
+    n_trs1 = size(params_mat1, 2);
+    n_trs2 = size(params_mat2, 2);
+    
     params_struc_fused_orig = params_struc_fused;
     n_trs = n_trs1 + n_trs2;
     r_vec = randperm(n_trs);
--- IDnF_rig_code_20171031/Olfactometer/NewOlfactometer/prime_olf2.m
+++ IDnF_rig_code_20171031/Olfactometer/NewOlfactometer/prime_olf2.m
@@ -8,14 +8,16 @@ end
 n_pulses = length(od_list).*2;
 
 pulse_train = zeros(n_pulses, 2) + 5;
+pulse_train(:, 2) = 8;
 od_vec = repmat(od_list, 1, 2);
 od_vec = reshape(od_vec, 1, []);
 
-olf_arduino_serial_comm(0, odor_train, odor_vec, 1);
+olf_arduino_serial_comm(0, pulse_train, od_vec, 1);
 pause(0.5)
 
 trigger_scan(1);
 pause(1)
 trigger_scan(0);
+pause(size(pulse_train, 1).*10);
 
 sleep_olf2;
