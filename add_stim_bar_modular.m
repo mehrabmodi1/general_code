c0b59ba25c1cc3026f8448c6a7f6c12329cac5d9
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2019-09-12 10:51:51 -0400

    started work on a version that can plot two bars for two olfactometers.

---
+++ add_stim_bar_modular.m
@@ -0,0 +1,73 @@
+ function [] = add_stim_bar(fig_n, stim_frames, stim_colors)
+%Syntax: [] = add_stim_bar(fig_n, stim_frames, stim_colors)
+%this function creates a fake, second pair of axes that is invisible, under
+%the first one, and creates a patch colored stim_color in it to denote the stimulus duration.
+%stim_colors is a matrix of color vecs with the number of rows equal to the
+%number of patches.
+
+%testing lines
+% clear all
+% close all
+% test_vec = rand(1, 100);
+% stim_frames = [20, 60];
+% stim_colors = [.3, .5, .7];
+% fig_n = 1;
+% figure(1);
+% plot(test_vec);
+
+%checking if stim_frames has two sets of stim frames specified, one for each
+%olfactometer or if it follows the old convention
+
+if iscell(stim_frames) == 1
+    if max(isnan(stim_frames{1, 2})) == 0
+        n_olfs = 2;
+    else
+        n_olfs = 1;
+        stim_frames = stim_frames{1, 1};
+    end
+else
+    n_olfs = 1;
+end
+
+disp('Run this function only AFTER fig_wrapup or the time-scale is distorted.');
+figure(fig_n);
+a = gca;
+orig_ax = axis;
+new_ax = [orig_ax(1:2), 0, 5];
+orig_pos = a.Position;
+new_pos = [orig_pos(1), (orig_pos(2) + orig_pos(4)), orig_pos(3), (orig_pos(4).*1.15) ];
+ax2 = axes('Position', new_pos);    %created new axes object in same figure
+axis(new_ax);
+set(ax2, 'Color', 'none', 'Visible', 'off');
+n_patches = size(stim_frames, 1);
+
+%replicating stim_color and using it for all patches if only one color is
+%specified for a pulse train
+if n_patches > 1 && size(stim_colors, 1) == 1
+    stim_colors = repmat(stim_colors, n_patches, 1);
+else
+end
+
+for olf_n = 1:n_olfs
+curr_stim_frs = stim_frames{1, olf_n};
+    offset = (olf_n - 1).*0.2;  %adding gap between rows of patches
+
+    for patch_n = 1:size(curr_stim_frs, 1)        %loop to draw multiple patches if needed
+        %patch height
+        y1 = .1 + offset;
+        y2 = .2 + offset;
+
+        %patch width
+        x1 = curr_stim_frs(patch_n, 1);
+        x2 = curr_stim_frs(patch_n, 2);
+
+        %drawing patch
+        y_vec = [y1, y2, y2, y1];
+        x_vec = [x1, x1, x2, x2];
+
+        p = patch(x_vec', y_vec', stim_colors(patch_n, :) );
+
+        p.EdgeColor = [1, 1, 1];
+        keyboard
+    end
+end
