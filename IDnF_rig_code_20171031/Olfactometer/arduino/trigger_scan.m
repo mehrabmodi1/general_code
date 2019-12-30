f1279944636dd0d503a5b4429002130e4ce103e8
mehrab modi <mehrabmodi@gmail.com>
2019-12-30 18:02:35 -0500

    updated to handle separate triggers for scan and olf2.

--- IDnF_rig_code_20171031/Olfactometer/arduino/trigger_scan.m
+++ IDnF_rig_code_20171031/Olfactometer/arduino/trigger_scan.m
@@ -1,10 +1,16 @@
-function [] = trigger_scan(write_val) 
+function [] = trigger_scan(write_val, write_val_olf2) 
 
 trig_ard_port = 'COM10';    %this is the serial port number for the trigger control arduino
 trig_line = 'D2';           %this is the digital line number connected to Scanimage to trigger acqn.
+trig_line_olf2 = 'D4';
 
 iscon = check_ser_connected(trig_ard_port);
+if nargin == 1
+    write_val_olf2 = write_val;
+else
+end
 
+keyboard
 %connecting to arduino if necessary
 persistent trig_ard
 if iscon == 0
@@ -21,6 +27,14 @@ try
             writeDigitalPin(trig_ard, trig_line, 0);
         else
         end
+        
+        if strcmp(write_val_olf2, 'high') == 1
+            writeDigitalPin(trig_ard, trig_line_olf2, 1);
+        elseif strcmp(write_val_olf2, 'low') == 1
+            writeDigitalPin(trig_ard, trig_line_olf2, 0);
+        else
+        end
+
 
     else
         if write_val == 1
@@ -31,6 +45,16 @@ try
         else
             error('acceptable inputs are 0, 1, ''low'' and ''high''. ')
         end
+        
+        if write_val_olf2 == 1
+            writeDigitalPin(trig_ard, trig_line_olf2, 1);
+
+        elseif write_val_olf2 == 0
+            writeDigitalPin(trig_ard, trig_line_olf2, 0);
+        else
+            error('acceptable inputs are 0, 1, ''low'' and ''high''. ')
+        end
+
 
     end
 catch
