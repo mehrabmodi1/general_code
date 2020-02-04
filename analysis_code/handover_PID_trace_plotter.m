d52fa39da8853478124d77a78a065c63c6f28735
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2020-02-03 22:37:21 -0500

    minor changes.

--- analysis_code/handover_PID_trace_plotter.m
+++ analysis_code/handover_PID_trace_plotter.m
@@ -1,7 +1,7 @@
 clear all
 close all
 
-curr_dir = 'C:\Data\Data\Raw_data\20200131\handover_PID_traces\';
+curr_dir = 'C:\Data\Data\Raw_data\20200203\handover_PID_traces_set2\';
 
 [del, odor_names1] = xlsread('C:\Data\Code\general_code_old\IDnF_rig_code_20171031\Olfactometer\NewOlfactometer\calibration\odorList.xls', 1);
 [del, odor_names2] = xlsread('C:\Data\Code\general_code_old\IDnF_rig_code_20171031\Olfactometer\NewOlfactometer\calibration\odorList_olf2.xls', 1);
@@ -11,6 +11,8 @@ odor_names2{3} = 'Butyl acetate';
 PA_color = color_vec(2, :);
 BA_color = color_vec(1, :);
 EL_color = color_vec(3, :);
+
+y_ax_lim = 0.04;
 
 %identifying stim_mat_simple col numbers
 led_on_col_n = find_stim_mat_simple_col('led_on', column_heads);            %identifying relevant column number in stim_mat_simple
@@ -27,34 +29,37 @@ odn_list_olf2(isnan(odn_list_olf2)) = [];
 
 %plotting simple odor pulse PID traces
 %PA
-plot_simple_traces(3, 1, PA_color, 1, 2, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+plot_simple_traces(3, 1, PA_color, 1, 2, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 
 %BA
-plot_simple_traces(10, 3, BA_color, 3, 4, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+plot_simple_traces(10, 3, BA_color, 3, 4, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 
 %EL
-plot_simple_traces(11, 4, EL_color, 5, 6, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+plot_simple_traces(11, 4, EL_color, 5, 6, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 
 %plotting handover odor pulse PID traces
+%BA-BA
+plot_hover_traces(10, 3, BA_color, BA_color, 13, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
+
 %PA-BA
-plot_hover_traces(3, 3, PA_color, BA_color, 7, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+plot_hover_traces(3, 3, PA_color, BA_color, 7, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 %PA-EL
-plot_hover_traces(3, 4, PA_color, EL_color, 8, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+plot_hover_traces(3, 4, PA_color, EL_color, 8, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 
 %BA-PA
-plot_hover_traces(10, 1, PA_color, BA_color, 9, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+plot_hover_traces(10, 1, BA_color, PA_color, 9, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 %BA-EL
-plot_hover_traces(10, 4, PA_color, EL_color, 10, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+plot_hover_traces(10, 4, BA_color, EL_color, 10, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 
 %EL-PA
-plot_hover_traces(11, 1, EL_color, PA_color, 11, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+plot_hover_traces(11, 1, EL_color, PA_color, 11, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 %EL-BA
-plot_hover_traces(11, 3, EL_color, BA_color, 12, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+plot_hover_traces(11, 3, EL_color, BA_color, 12, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 
 
 
 
-function [] = plot_simple_traces(olf1_odn, olf2_odn, od_color, fign1, fign2, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+function [] = plot_simple_traces(olf1_odn, olf2_odn, od_color, fign1, fign2, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 frame_time = 0.099;
 
 %list of all handover trials, with any combination of odors
@@ -64,13 +69,17 @@ handover_trs = find(stim_mat_simple(:, dur_col_ns(1)) == 10 & stim_mat_simple(:,
 curr_trs = find(stim_mat_simple(:, od_col_ns(1)) == olf1_odn & stim_mat_simple(:, dur_col_ns(1)) == 10);
 [del1, del] = intersect(curr_trs, handover_trs);
 curr_trs(del) = [];
-curr_traces = get_PID_traces(curr_dir, curr_trs, frame_time);
+curr_traces = get_PID_traces(curr_dir, curr_trs, frame_time, 0);
+curr_traces(:, 1:2:end) = curr_traces(:, 1:2:end) - curr_traces(:, 2:2:end); 
 curr_traces = curr_traces(1:(end - 5), 1:2:end);  %getting rid of LED traces
 stim_frs = compute_stim_frs_modular(stim_mat, curr_trs(1), frame_time);
 stim_frs = [stim_frs{1}]; 
 figure(fign1)
 plot(curr_traces, 'lineWidth', 2, 'Color', [0.65, 0.65, 0.65]);
 ylabel('PID signal (V)');
+ax_vals = axis;
+ax_vals(4) = y_ax_lim;
+axis(ax_vals);
 set_xlabels_time(fign1, frame_time, 10);
 fig_wrapup(fign1, []);
 add_stim_bar(fign1, stim_frs, od_color);
@@ -79,14 +88,18 @@ add_stim_bar(fign1, stim_frs, od_color);
 curr_trs = find(stim_mat_simple(:, od_col_ns(2)) == olf2_odn & stim_mat_simple(:, dur_col_ns(2)) == 10);
 [del1, del] = intersect(curr_trs, handover_trs);
 curr_trs(del) = [];
-curr_traces = get_PID_traces(curr_dir, curr_trs, frame_time);
+curr_trs(1) = [];
+curr_traces = get_PID_traces(curr_dir, curr_trs, frame_time, 0);
+curr_traces(:, 1:2:end) = curr_traces(:, 1:2:end) - curr_traces(:, 2:2:end); 
 curr_traces = curr_traces(:, 1:2:end);  %getting rid of LED traces
 stim_frs = compute_stim_frs_modular(stim_mat, curr_trs(1), frame_time);
 stim_frs = [stim_frs{2}]; 
 figure(fign2)
 plot(curr_traces, 'lineWidth', 2, 'Color', [0.65, 0.65, 0.65]);
 ylabel('PID signal (V)');
-%title ('Pentyl acetate, olfactometer1')
+ax_vals = axis;
+ax_vals(4) = y_ax_lim;
+axis(ax_vals);
 set_xlabels_time(fign2, frame_time, 10);
 fig_wrapup(fign2, []);
 add_stim_bar(fign2, stim_frs, od_color);
@@ -94,7 +107,7 @@ add_stim_bar(fign2, stim_frs, od_color);
 end
 
 
-function [] = plot_hover_traces(olf1_odn, olf2_odn, od1_color, od2_color, fign, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir)
+function [] = plot_hover_traces(olf1_odn, olf2_odn, od1_color, od2_color, fign, stim_mat, stim_mat_simple, od_col_ns, dur_col_ns, curr_dir, y_ax_lim)
 frame_time = 0.099;
 
 %list of all handover trials, with any combination of odors
@@ -102,15 +115,18 @@ handover_trs = find(stim_mat_simple(:, dur_col_ns(1)) == 10 & stim_mat_simple(:,
 
 curr_trs = find(stim_mat_simple(:, od_col_ns(1)) == olf1_odn & stim_mat_simple(:, dur_col_ns(1)) == 10 ...
     & stim_mat_simple(:, od_col_ns(2)) == olf2_odn & stim_mat_simple(:, dur_col_ns(2)) == 10);
-
-curr_traces = get_PID_traces(curr_dir, curr_trs, frame_time);
+curr_trs(1) = [];
+curr_traces = get_PID_traces(curr_dir, curr_trs, frame_time, 0);
+curr_traces(:, 1:2:end) = curr_traces(:, 1:2:end) - curr_traces(:, 2:2:end); 
 curr_traces = curr_traces(:, 1:2:end);  %getting rid of LED traces
 stim_frs = compute_stim_frs_modular(stim_mat, curr_trs(1), frame_time);
 stim_frs = [stim_frs{1}; stim_frs{2}]; 
 figure(fign)
 plot(curr_traces, 'lineWidth', 2, 'Color', [0.65, 0.65, 0.65]);
 ylabel('PID signal (V)');
-%title ('Pentyl acetate, olfactometer1')
+ax_vals = axis;
+ax_vals(4) = y_ax_lim;
+axis(ax_vals);
 set_xlabels_time(fign, frame_time, 10);
 fig_wrapup(fign, []);
 add_stim_bar(fign, stim_frs, [od1_color; od2_color]);
