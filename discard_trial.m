9fd69b36fcf4cd7b65e7e69d46a8a0de6d01e3ed
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2020-02-18 16:19:05 -0500

    function to discard trial and overwrite saved extracted_data_mat files.

---
+++ discard_trial.m
@@ -0,0 +1,22 @@
+function [] = discard_trial(direc, trial_n)
+%syntax: function [] = discard_trial(direc, trial_n)
+%This function allows the user to manually identify trials with issues
+%(movement etc)and discard them in the saved_traces .mat file
+
+direc = 'C:\Data\Data\Analysed_data\Suite2P_results\20200103\fly1_c739_handoverPaBaEl\\\1\';
+extracted_raw_data_mat = load([direc, 'extracted_raw_data_mat.mat']);
+raw_data_mat = extracted_raw_data_mat.raw_data_mat;
+raw_data_mat_orig = raw_data_mat;
+
+if isempty(trial_n) == 0
+    raw_data_mat(:, :, trial_n) = nan;
+elseif isempty(trial_n) == 1    %case where user wants to revert to original, full raw_data_mat
+    extracted_raw_data_mat = load([direc, 'extracted_raw_data_mat_orig.mat']);
+    raw_data_mat = extracted_raw_data_mat.raw_data_mat_orig;
+    raw_data_mat_orig = raw_data_mat;
+else
+end
+    
+%saving
+save([direc, 'extracted_raw_data_mat.mat'], 'raw_data_mat');
+save([direc, 'extracted_raw_data_mat_orig.mat'], 'raw_data_mat_orig');
