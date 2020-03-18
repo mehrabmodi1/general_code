3c923a59a33b0a6ffdbc85796cad7e432c67a9b5
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2020-03-18 14:22:05 -0400

    started work on adapting Herve's fitting code to run generally.

---
+++ Herve_fits/test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 11:13:39 2020
+
+@author: Mehrab
+"""
+
+
+act_fn = "blah/blah/blah/{}/fly{}/dFF_data.mat".format('Gamma', '1');
+breakpoint()
+print(act_fn)
+lobe = 'Gamma1';
+fly = 1;
+
+folder_fit = "C:/Data/Data/Analysed_data/data_sharing/KC_param_fitting_sandbox/fits_{}_fly{}".format(lobe, fly)
