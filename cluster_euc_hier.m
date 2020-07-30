707a8c9ac43db6a7f06bb826a229b293bb4741a8
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2020-07-30 17:18:04 -0400

    Switched to clustering KCs based only on z-scored taus.

--- cluster_euc_hier.m
+++ cluster_euc_hier.m
@@ -1,4 +1,4 @@
-function [data_mat_sorted, label_mat_sorted, clust_ids, link_map] = cluster_euc_hier(data_mat, label_mat, n_clusts)
+function [data_mat_sorted, data_mat_sorted_n, label_mat_sorted, clust_ids, link_map] = cluster_euc_hier(data_mat, label_mat, n_clusts)
 %Syntax:[corr_mat_sorted, clust_ids, link_map] = cluster_euc_hier(data_mat, n_clusts)
 %This function takes a data matrix with nrows observations and ncols
 %dimensions and clusters observations by their euclidean distances with the 
@@ -7,6 +7,14 @@ function [data_mat_sorted, label_mat_sorted, clust_ids, link_map] = cluster_euc_
 %corrcoef matrix for display, along with cluster ids and the linkage map.
 %labelmap is a matrix of nrows = nrows in data_mat. It is an arbitrary set
 %of labels to be co-sorted along with the observations.
+
+%normalising columns of data_mat by computing z-scores
+mean_vec = mean(data_mat, 'omitnan');
+sd_vec = std(data_mat, [], 'omitnan');
+data_mat_orig = data_mat;
+data_mat = data_mat - repmat(mean_vec, size(data_mat, 1), 1);
+data_mat = data_mat ./ repmat(sd_vec, size(data_mat, 1), 1);
+
 
 link_map = linkage(data_mat, 'centroid');
 clust_ids = cluster(link_map, 'maxclust', n_clusts);          %grouping cells into a maximum of 5 clusters
@@ -29,16 +37,22 @@ end
 
 %re-ordering observations acc to cluster identity
 data_mat_sorted = [];
+data_mat_sorted_n = [];
 label_mat_sorted = [];
 clust_ids_sorted = [];
 for clust_n = 1:n_clusts
     curr_members = find(clust_ids == clust_n);
     curr_labels = label_mat(curr_members, :);   %label vectors for currently cluster's observations
-    curr_obs = data_mat(curr_members, :);       %observation vectors for currently cluster's observations
-    ave_obs = mean(curr_obs, 'omitnan');    
+    curr_obs = data_mat_orig(curr_members, :);       %observation vectors for currently cluster's observations
+    curr_obs_n = data_mat(curr_members, :);       %observation vectors for currently cluster's observations
+    ave_obs = mean(curr_obs, 1, 'omitnan');    
     
     %sorting within current cluster
-    corrs = corrcoef([ave_obs; curr_obs]');
+    try
+        corrs = corrcoef([ave_obs; curr_obs]');
+    catch
+        keyboard
+    end
     corrs = corrs(2:end, 1);
    
     curr_obs = [corrs, curr_obs];
@@ -49,7 +63,10 @@ for clust_n = 1:n_clusts
     curr_labels = sortrows(curr_labels, -1);
     label_mat_sorted = [label_mat_sorted; curr_labels(:, 2:end)];
     
-    
+    curr_obs_n = [corrs, curr_obs_n];
+    curr_obs_n = sortrows(curr_obs_n, -1);
+    data_mat_sorted_n = [data_mat_sorted_n; curr_obs_n(:, 2:end)];
+        
     clust_ids_sorted = [clust_ids_sorted; (zeros(length(curr_members), 1) + clust_n) ];
 end
 clust_ids = clust_ids_sorted;
