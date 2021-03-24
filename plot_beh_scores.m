cd8c40556d7da11e06c95c7d2bef742a7be28050
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2021-03-24 15:02:29 -0400

    Now also generates a plot for easy, hard and generalized discrimination for MB296B. Data missing for PA-BA discrimination.

--- plot_beh_scores.m
+++ plot_beh_scores.m
@@ -4,7 +4,7 @@ close all
 path = 'C:\Users\Mehrab\Google Drive\Backup\Stuff\Janelia\Talks\DIG\20201009\MB099C_data.xls';  %MBONG2A'1 dataset
 gen_path = 'C:\Users\Mehrab\Google Drive\Backup\Stuff\Janelia\Talks\Labmeeting talks\20201218\MB099C_generalization_data.xls'; %Aditya's generalization dataset    
 scr_path = 'C:\Users\Mehrab\Google Drive\Backup\Stuff\Janelia\Talks\Labmeeting talks\20201218\Yoshi_cpt_screen.xls';    %Yoshi's US-subst screen across cpts
-
+MB296B_genpath = 'C:\Users\Mehrab\Google Drive\Backup\Stuff\Janelia\Talks\Labmeeting talks\20201218\MB296B_generalization_data.xls';
 
 %Plotting fine discr and coarse discr for multiple compartments
 [scr_data] = xlsread(scr_path, 1).*-1;
@@ -14,15 +14,15 @@ col_pairs = [];
 %Note: this data is for these drivers: MB320C(G1pedc), MB320C, MB099C(G2A'1), MB099C, MB630B(A3), MB630B, MB043C(A1), MB043C, MB213B(B1B2), MB213B,  
 xlabels = [{'PA-EL (G1pedc)'}, {'PA-BA'}, {'PA-EL (G2A''1)'}, {'PA-BA'}, {'PA-EL (A3)'}, {'PA-BA'}, {'PA-EL (A1)'}, {'PA-BA'}, {'PA-EL (B1B2)'}, {'PA-BA'}] ;
 figure(1)
-fig_h = scattered_dot_plot_ttest(scr_data, 1, 1, 4, 8, marker_colors, 1, col_pairs, line_colors, xlabels, 1, [0.8, 0.4, 0.4], 1, 0.05);
-holf on
+fig_h = scattered_dot_plot_ttest(scr_data, 1, 1, 4, 8, marker_colors, 1, col_pairs, line_colors, xlabels, 1, [0.8, 0.4, 0.4], 2, 0.05);
+hold on
 ax_vals = axis;
-plot([0, ax_vals(2)], [0, 0], 'Color', [0.65, 0.65, 0.65], );
+plot([0, ax_vals(2)], [0, 0], 'Color', [0.65, 0.65, 0.65]);
 ylabel('performance index');
 fig_wrapup(fig_h, []);
 
 
-%Plotting fine discr and generalization for MB099C data
+%Plotting fine discr and generalization for MB099C data (G2A'1 + others)
 [beh_data] = xlsread(path, 1).*-1;
 [gen_data] = xlsread(gen_path, 1);
 
@@ -32,7 +32,23 @@ line_colors = [];
 col_pairs = [];
 xlabels = [{'PA-EL'}, {'PA-BA'}, {'BA-EL'}];
 figure(2)
-fig_h = scattered_dot_plot_ttest(beh_data, 2, 1, 4, 8, marker_colors, 1, col_pairs, line_colors, xlabels, 1, [0.8, 0.4, 0.4], 1, 0.05);
+fig_h = scattered_dot_plot_ttest(beh_data, 2, 1, 4, 8, marker_colors, 1, col_pairs, line_colors, xlabels, 1, [0.8, 0.4, 0.4], 2, 0.05);
+ylabel('performance index');
+fig_wrapup(fig_h, []);
+
+ax_vals = axis;
+ax_vals(4) = 1.1;
+axis(ax_vals);
+
+
+%Plotting fine discr and generalization for MB296B data (only G2A'1)
+[gen_data] = xlsread(MB296B_genpath, 1);
+marker_colors = repmat([0.65, 0.65, 0.65], 3, 1);
+line_colors = [];
+col_pairs = [];
+xlabels = [{'PA-EL'}, {'PA-BA'}, {'BA-EL'}];
+figure(3)
+fig_h = scattered_dot_plot_ttest(gen_data, 3, 1, 4, 8, marker_colors, 1, col_pairs, line_colors, xlabels, 1, [0.8, 0.4, 0.4], 2, 0.05);
 ylabel('performance index');
 fig_wrapup(fig_h, []);
 
@@ -42,3 +58,4 @@ axis(ax_vals);
 
 %one-sample ttesting
 [h, p] = ttest(beh_data)    %checking if different from 0
+[h, p] = ttest(scr_data) 
