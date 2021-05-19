748fa2d6d2b175756856dce8020c34336055bfc0
mehrabmodi@gmail.com <mehrabmodi@gmail.com>
2021-05-19 17:33:38 -0400

    implemented distance metric and PCA plots

--- analysis_code/KC_d5HT1b_transition_pattern_an.m
+++ analysis_code/KC_d5HT1b_transition_pattern_an.m
@@ -2,11 +2,10 @@ clear all
 close all
 
 dataset_list_paths = [ ...
-                         {'C:\Data\Code\general_code_old\data_folder_lists\Janelia\d5HT1b_similar_od_handovers.xls'};...
-                      
+                         %{'C:\Data\Code\general_code_old\data_folder_lists\Janelia\d5HT1b_similar_od_handovers.xls'};...
+                         {'C:\Data\Code\general_code_old\data_folder_lists\Janelia\c305a_similar_od_handovers.xls'};...
                       ];
             
-suppress_plots = [];
 [del, odor_names1] = xlsread('C:\Data\Code\general_code_old\IDnF_rig_code_20171031\Olfactometer\NewOlfactometer\calibration\odorList.xls', 1);
 [del, odor_names2] = xlsread('C:\Data\Code\general_code_old\IDnF_rig_code_20171031\Olfactometer\NewOlfactometer\calibration\odorList_olf2.xls', 1);
 odor_names2{3} = 'Butyl acetate';
@@ -20,6 +19,8 @@ BA_color = [0.5549, 0.9686, 0.433].*0.8;
 mean_color = ([0, 49, 152]./256).*1.5;
 
 
+suppress_QC_plots = 1;
+
 global color_vec;                
 a = colormap('bone');
 global greymap
@@ -31,17 +32,19 @@ y_ax_traces = 0.8;
 y_ax_fit_traces = 0.6;
 saved_long_traces = 0;
 all_sig_frs = [];
+pause_PCAs = 0;
 for list_n = 1:size(dataset_list_paths, 1)
     curr_dir_list_path = dataset_list_paths{list_n, 1};
     [del, dir_list] = xlsread(curr_dir_list_path, 1);        %list of Suite2P results directories
     n_dirs = size(dir_list, 1);
     dataset_list_name = findstr(curr_dir_list_path, 'list_');
     dataset_list_name = curr_dir_list_path((dataset_list_name + 5):(end - 4));
-    
-    
+        
     %loop to go through all experiment datasets listed in list file
     all_resp_traces = [];
     sig_cell_mat_all = [];
+    saved_resp_sizes_all = [];
+    n_cells_all = [];
     for dir_n = 1:n_dirs
         fly_n = fly_n + 1;
               
@@ -52,14 +55,14 @@ for list_n = 1:size(dataset_list_paths, 1)
        
         tif_times = load([curr_dir, 'tif_time_stamps.mat']);           %reading in time stamps for each tif file recorded by raw_data_extracter
         tif_times = tif_times.time_stamps;
+        
         cd(curr_dir);
         tif_name = dir('*.tif');
         stack_obj = ScanImageTiffReader([curr_dir, tif_name(1).name]);
         [frame_time, zoom, n_chans, PMT_offsets] = SI_tif_info(stack_obj);
-        
         [stim_mat, stim_mat_simple, column_heads, color_vec, good_tr_list, params_orig, PID_traces_matched, PID_traces_orig] = load_params_trains_modular(curr_dir, tif_times, frame_time);    %reading in trial stimulus parameters after matching time stamps to F traces
         color_vec = [PA_color; BA_color; EL_color];
-       
+        
         %Reading in experimental parameters
         odor_list_olf1 = unique(stim_mat_simple(:, 1) );
         n_odors_olf1 = length(odor_list_olf1);
@@ -96,7 +99,6 @@ for list_n = 1:size(dataset_list_paths, 1)
         filt_time = .5;            %in s, the time window for boxcar filter for generating filtered traces
         [dff_data_mat, dff_data_mat_f] = cal_dff_traces_res(raw_data_mat, stim_mat, frame_time, filt_time, curr_dir, []);
         
-        
         del = find(dff_data_mat_f < -1);
         dff_data_mat_f(del) = -1;       %forcing crazy values to sane ones
         
@@ -127,7 +129,7 @@ for list_n = 1:size(dataset_list_paths, 1)
         [resp_sizes, sig_trace_mat, sig_cell_mat, sig_cell_mat_key, resp_areaundercurves] = cal_sig_responses_res_modular(dff_data_mat, stim_mat, stim_mat_simple, frame_time, od_col_ns, dur_col_ns);
         
         %sanity-checking KC data
-        [del, all_bad_trs] = cell_data_quality_control(dff_data_mat_f, stim_mat, stim_mat_simple, column_heads, sig_cell_mat, sig_cell_mat_key, 1, frame_time);
+        [del, all_bad_trs] = cell_data_quality_control(dff_data_mat_f, stim_mat, stim_mat_simple, column_heads, sig_cell_mat, sig_cell_mat_key, suppress_QC_plots, frame_time);
                 
         union_sig_cell_vec = sum(sig_cell_mat, 2);
         union_sig_cell_vec = sign(union_sig_cell_vec);
@@ -139,6 +141,7 @@ for list_n = 1:size(dataset_list_paths, 1)
         %computing mean response trace for each simple odor stimulus presented
         resp_trace_mat = [];
         bad_cells_all = [];
+        saved_resp_sizes = [];
         for stim_type_n = 1:size(sig_cell_mat_key, 1)
             curr_stim_vec = sig_cell_mat_key(stim_type_n, :);
             
@@ -165,33 +168,74 @@ for list_n = 1:size(dataset_list_paths, 1)
             stim_frs = compute_stim_frs_modular(stim_mat, curr_trs(1), frame_time);  %computing stimulus on and off frame numbers for olf1 and olf2
             
             curr_mean_traces = squeeze(mean(dff_data_mat_f(:, union_sig_cells, curr_trs), 3, 'omitnan'));
-%             del = curr_mean_traces < 0;
-%             curr_mean_traces(del) = 0;
             
-%             curr_sds = std(curr_mean_traces((stim_frs(1) - round(10./frame_time)):(stim_frs(1) - 2), :), [], 1, 'omitnan');
-%             curr_means = mean(curr_mean_traces((stim_frs(1) - round(5./frame_time)):(stim_frs(1) - 2), :), 1, 'omitnan');
             
-
-
+            %computing response sizes and logging only for sig cells
+            stim_frs_simp = stim_frs{2};
+            stim_frs_simp(2) = stim_frs_simp(2) + round(2./frame_time);
+            mean_resps = mean(mean(dff_data_mat(stim_frs_simp(1):stim_frs_simp(2), :, curr_trs), 3, 'omitnan'), 1);
+            nonsig_cells = find(sig_cell_mat(:, stim_type_n) == 0);
+            mean_resps(nonsig_cells) = 0;
+            saved_resp_sizes(:, stim_type_n) = mean_resps';
+            
+            
+            %Logging single trial responses to compute PC representations later on
+            curr_resps_pca = squeeze(mean(dff_data_mat(stim_frs_simp(1):stim_frs_simp(2), :, curr_trs), 'omitnan'));
+            curr_insig_cells = find(sig_cell_mat(:, stim_type_n) == 0);
+            curr_resps_pca(curr_insig_cells, :) = 0;
+            saved_resps_pca(:, :, stim_type_n) = curr_resps_pca;   
+            
             %identifying mis-identified junk cells
             stim_on = stim_frs{1};
             stim_on = stim_on(1) - 1;
             bad_cells = mean(abs(curr_mean_traces(1:stim_on, :)), 1, 'omitnan');
-            bad_cells = find(bad_cells > 0.10);
+            bad_cells = find(bad_cells > 0.50);
             bad_cells_all = [bad_cells_all, bad_cells];
             
             %logging current fly's response traces
-            resp_trace_mat = pad_n_concatenate(resp_trace_mat, curr_mean_traces, 3, nan);
-            
-            
+            resp_trace_mat = pad_n_concatenate(resp_trace_mat, curr_mean_traces, 3, nan);            
         end
+        
         %getting rid of misidentified junk cells
         resp_trace_mat(:, bad_cells_all, :) = [];
         bad_cells_all = [];
         
         %pooling sig cells across flies
         all_resp_traces = pad_n_concatenate(all_resp_traces, resp_trace_mat, 2, nan);
+        saved_resp_sizes_all = [saved_resp_sizes_all; saved_resp_sizes];
+        n_cells_all = [n_cells_all; n_cells];
         disp(curr_dir)
+        
+        
+        %computing and plotting PC representations for current fly
+        ave_resp_mat = squeeze(mean(saved_resps_pca, 2, 'omitnan'));
+        all_data_mat = reshape(saved_resps_pca, size(saved_resps_pca, 1), (size(saved_resps_pca, 2).*size(saved_resps_pca, 3)));
+        PC_wts = pca(all_data_mat');
+        
+        PC1_resps = squeeze(sum(saved_resps_pca.*repmat(PC_wts(:, 1), 1, size(saved_resps_pca, 2), size(saved_resps_pca, 3)), 1, 'omitnan'));
+        PC2_resps = squeeze(sum(saved_resps_pca.*repmat(PC_wts(:, 2), 1, size(saved_resps_pca, 2), size(saved_resps_pca, 3)), 1, 'omitnan'));
+        
+%         mean_PC1_resps = squeeze(sum(ave_resp_mat.*repmat(PC_wts(:, 1), 1, size(ave_resp_mat, 2)), 1, 'omitnan'));
+%         mean_PC2_resps = squeeze(sum(ave_resp_mat.*repmat(PC_wts(:, 2), 1, size(ave_resp_mat, 2)), 1, 'omitnan'));
+        
+        color_vecs = [PA_color; BA_color; EL_color];
+        %plotting in PC space
+        for od_n = 1:3
+            figure(14)
+            plot(PC1_resps(:, od_n), PC2_resps(:, od_n), '.', 'Color', color_vecs(od_n, :), 'markerSize', 18);
+            hold on
+            %plot(mean_PC1_resps(od_n), PC2_resps(od_n), '.', 'Color', color_vecs(od_n, :).*0.75, 'markerSize', 24);
+            
+        end
+        xlabel('PC1')
+        ylabel('PC2')
+        hold off
+        fig_wrapup(14, [])
+        if pause_PCAs == 1
+            keyboard
+        else
+        end
+        saved_resps_pca = [];
         
      end
      
@@ -263,24 +307,25 @@ for list_n = 1:size(dataset_list_paths, 1)
             od_name_vec = [od_name_vec; {[olf2_od_name, ' transition']}];
         else
         end
+        
         q_resp_mat = [q_resp_mat; q_resp_vec];
        
         
         fig_h = figure('Name', curr_stim_name);
         imagesc(curr_mean_traces', [0, 1.5]);
         colormap(greymap);
-        set_xlabels_time(fig_h, frame_time, 10);
+        set_xlabels_time(fig_h, frame_time, 15);
         ylabel('cell number');
-        fig_wrapup(fig_h, []);
+        fig_wrapup_mod(fig_h, 'tall', []);
         add_stim_bar(fig_h, stim_frs, curr_color);
     end
     
-     
     %plotting scatter plots and computing corrcoefs
     [corr_mat, p_mat] = corrcoef(q_resp_mat');
     
     pair_list = [1, 2;...   %1. Simple PA - Simple BA
                  1, 3;...   %2. Simple PA - Simple EL
+                 
                  2, 3;...   %3. Simple BA - Simple EL
                  5, 4;...   %4. Transition PA - Transition BA
                  1, 5;...   %4. Simple PA - Transition PA
@@ -307,8 +352,7 @@ for list_n = 1:size(dataset_list_paths, 1)
         fig_wrapup(fig_h, []);
     end
     
-    
-    
+        
     %generating venn diagram of significant responders to each odor (simple stimuli)
     %computing counts
     PA_ct = sum(sig_cell_mat_all(:, 1));
@@ -339,6 +383,49 @@ for list_n = 1:size(dataset_list_paths, 1)
         text(S.ZoneCentroid(i,1), S.ZoneCentroid(i,2), curr_name)
     end
     
+    
+    %computing mean Euclidean distances between odors for each fly 
+    dists_saved = zeros(size(n_cells_all, 1), 4) + nan;
+    for fly_n = 1:size(n_cells_all, 1)
+        if fly_n > 1
+            curr_celln_st = sum(n_cells_all(1:(fly_n - 1))) + 1; 
+        else
+            curr_celln_st = 1;
+        end
+        curr_celln_end = sum(n_cells_all(1:fly_n));
+        
+        curr_resp_mat = saved_resp_sizes_all(curr_celln_st:curr_celln_end, :)';
+        pad = zeros(1, size(curr_resp_mat, 2));
+        curr_resp_mat_p = [pad; curr_resp_mat];   %adding a point for the origin        
+        dist_mat = pdist(curr_resp_mat_p);
+        dist_mat = squareform(dist_mat);
+        %normalizing all distances to distance of EL from origin
+        dist_mat = dist_mat./(dist_mat(1, 2));
+        dists_saved(fly_n, :) = [dist_mat(2, 3), dist_mat(2, 4), dist_mat(3, 4), dist_mat(5, 6)];     %PA-BAsim, PA-ELsim, BA-ELsim, BAPA - PABA
+        
+    end
+    
+    %plotting Euclidean distances
+    marker_colors = repmat([0.6, 0.6, 0.6], 4, 1);
+    col_pairs = [];
+    line_colors = marker_colors;
+    mean_color = [0.8, 0.4, 0.4];
+    xlabels = [{'PA-BA', 'PA-EL', 'BA-EL', 'BAPA-PABA'}];
+    fig_h = scattered_dot_plot_ttest(dists_saved(:, 1:3), 13, 2.5, 4, 6.5, marker_colors, 1, col_pairs, line_colors, xlabels, 2, mean_color, 2, 0.05, 0);
+    ylabel('norm. Euclidean dist.')
+    fig_wrapup(fig_h, []);
+    
+    %plotting Euclidean distances with transition resp distances
+    figure(15)
+    marker_colors = repmat([0.6, 0.6, 0.6], 4, 1);
+    col_pairs = [];
+    line_colors = marker_colors;
+    mean_color = [0.8, 0.4, 0.4];
+    xlabels = [{'PA-BA', 'BAPA-PABA'}];
+    fig_h = scattered_dot_plot_ttest(dists_saved(:, [1,4]), 13, 2.5, 4, 6.5, marker_colors, 1, col_pairs, line_colors, xlabels, 2, mean_color, 2, 0.05, 0);
+    ylabel('norm. Euclidean dist.')
+    fig_wrapup(fig_h, []);
+   
     
 end
 
