b1c6a029e04bc820d5aa59a4849aa2da6e1a839b
mehrabmodi <mehrabmodi@gmail.com>
2021-10-01 16:09:01 -0400

    added ability to specify scale bar color.

--- add_scale_bar.m
+++ add_scale_bar.m
@@ -8,16 +8,17 @@ bar_size = round((1./scale_micronsperpixel).*barsize_microns);
 
 %computing location of bar
 y1 = size(im_matrix_in, 2).*0.865;
-y2 = size(im_matrix_in, 2).*0.9;
+y2 = size(im_matrix_in, 2).*0.885;
 
 x2 = size(im_matrix_in, 1).*0.85;
 x1 = x2 - bar_size;
 
-if bar_colour == 1
+if bar_colour(1) == 1
     bar_val = max(max(im_matrix_in)).*1.05;
-elseif bar_colour == 0
+elseif bar_colour(1) == 0
     bar_val = min(max(im_matrix_in)).*0.95;
-else
+elseif bar_colour(1) == 2        %case where bar color is explicitly specified
+    bar_val = bar_colour(2);
 end
     
 im_matrix_out = im_matrix_in;
